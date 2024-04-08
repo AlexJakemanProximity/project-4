f4d6dfcfddf342c78d3f2dce7eef2680a43fdd28
AlexJakeman <Alexjakeman4@gmail.com>
2024-04-08 23:28:56 +0100

    Added admin fields

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import uuid
 
 from django.db import models
@@ -19,9 +20,9 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=Decimal('0.00'))
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=Decimal('0.00'))
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=Decimal('0.00'))
 
     def _generate_order_number(self):
         """
@@ -34,7 +35,9 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        lineitems_aggregate = self.lineitems.aggregate(Sum('lineitem_total')) # type: ignore
+        sum_of_lineitems = lineitems_aggregate['lineitem_total__sum'] if lineitems_aggregate['lineitem_total__sum'] else 0
+        self.order_total = sum_of_lineitems
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -54,7 +57,6 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
-
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
@@ -72,4 +74,3 @@ class OrderLineItem(models.Model):
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
-    
