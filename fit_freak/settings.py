5f432d433ecaebf67fca138a78575d87cc8828ab
AlexJakemanProximity <150019885+AlexJakemanProximity@users.noreply.github.com>
2024-04-11 23:49:14 +0100

    minor changes to settings and pip freeze

--- fit_freak/settings.py
+++ fit_freak/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
-        # Other
     'crispy_forms',
 ]
 
@@ -68,16 +67,12 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
-            # 'builtins': [
-            #     'crispy_forms.templatetags.crispy_forms_tags',
-            #     'crispy_forms.templatetags.crispy_forms_field',
-            # ]
         },
     },
 ]
