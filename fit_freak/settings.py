5970c19fe61e683cc0e3feca5f22a19a8877b1ba
AlexJakemanProximity <150019885+AlexJakemanProximity@users.noreply.github.com>
2024-04-12 00:52:00 +0100

    Updated allowed hosts

--- fit_freak/settings.py
+++ fit_freak/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-4v=86d9@0gi9gbvk!7((z#e1$0m1a#p1s7*zs1)#(ptf$9rfq+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['fit-freak.herokuapp.com', 'localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['fit-freak.herokuapp.com', 'localhost', 'http://localhost:8000', '.herokuapp.com']
 
 # Application definition
 
